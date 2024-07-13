c0c0e5f5e504385e64c73b013808dabdb2cc2056
Brandon Rodriguez <chilebr99@gmail.com>
2024-07-13 16:05:24 -0400

    User can display json output as raw or prettier json

--- SwiftSendLibrary/ApiAccess.cs
+++ SwiftSendLibrary/ApiAccess.cs
@@ -1,15 +1,21 @@
-﻿namespace SwiftSendLibrary;
+﻿using System.Text.Json;
+
+namespace SwiftSendLibrary;
 
 public class ApiAccess
 {
     private readonly HttpClient client = new();
-    public async Task<string> CallApiAsync(string url)
+    public async Task<string> CallApiAsync(string url, bool formatOutput = true)
     {
         var response = await client.GetAsync(url);
 
         if (response.IsSuccessStatusCode)
         {
             string json = await response.Content.ReadAsStringAsync();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(json);
+            json = JsonSerializer.Serialize(jsonElement,
+                new JsonSerializerOptions { WriteIndented = true });
+
             return json;
         }
         else
